2de00427731d07a2500fb5af3e77600c7e07d381
chaveroricardo <chavero.ricardo@gmail.com>
2019-02-12 22:05:36 -0600

    cambios

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+$(document).ready(function () {
 
-  console.log('IronGenerator JS imported successfully!');
+  $("#sidebar").mCustomScrollbar({
+       theme: "minimal"
+  });
 
-}, false);
+  $('#sidebarCollapse').on('click', function () {
+      // open or close navbar
+      $('#sidebar').toggleClass('active');
+      // close dropdowns
+      $('.collapse.in').toggleClass('in');
+      // and also adjust aria-expanded attributes we use for the open/closed arrows
+      // in our CSS
+      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  });
+
+});
